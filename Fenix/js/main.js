499db50d5c390f9c2905d53b11e1fbe8068d56c8
Pyrex <retojonatan@gmail.com>
2020-07-24 02:28:22 -0300

    cambios de carpetas y css

--- Fenix/js/main.js
+++ Fenix/js/main.js
@@ -1,3 +1,36 @@
+// FORMULARIO
+function contactForm() {
+    var nombre = document.getElementById("nombre").value;
+    var email = document.getElementById("email").value;
+    var asunto = document.getElementById("asunto").value;
+    var mensaje = document.getElementById("mensaje").value;
+    envioFormulario(nombre, email, asunto, mensaje);
+}
+
+function envioFormulario(nombre, email, asunto, mensaje) {
+    var urlCompleta = "https://www.farmaciafenix.com.ar/OperacionFormulario/MensajeContacto";
+
+    var request = $.ajax({
+        url: urlCompleta,
+        type: 'GET',
+        dataType: 'json',
+        data: {
+            nombre: nombre,
+            email: email,
+            asunto: asunto,
+            mensaje: mensaje
+        }
+    })
+    request.done(function (response) {
+        document.getElementById("form-contacto").reset();
+        $('#modalExito').modal('show');
+    })
+    request.fail(function (jqXHR, textStatus) {
+        document.getElementById("form-contacto").reset();
+        $('#modalFail').modal('show');
+    })
+}
+
 // SMOOTH SCROLLING
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
