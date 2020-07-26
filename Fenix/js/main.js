da7ac1b1b09c03330abfa5ab0811f5bec2fec20d
Pyrex <retojonatan@gmail.com>
2020-07-26 14:37:14 -0300

    responsive css y cambios en carrusel

--- Fenix/js/main.js
+++ Fenix/js/main.js
@@ -72,10 +72,26 @@ $('.carru-prod').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     dots: false,
-    arrows: true,
+    arrows: false,
     autoplay: true,
     draggable: true,
     infinite: true,
-    autoplaySpeed: 3000,
-    speed: 2000
+    autoplaySpeed: 4000,
+    speed: 2000,
+    responsive: [{
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        },
+        {
+            breakpoint: 1008,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                arrows: true,
+            }
+        }
+    ]
 });
