112abb791a17c5e4141340797fe29a214b2cf8f5
retojonatan <retojonatan@gmail.com>
2020-08-06 12:03:42 -0300

    cambios css responsive cambios id categories y js

--- bac-template/scripts/main.js
+++ bac-template/scripts/main.js
@@ -94,7 +94,7 @@ const selectImg = e => {
 var wow = new WOW({
   boxClass: 'wow', // animated element css class (default is wow)
   animateClass: 'animate__animated', // animation css class (default is animated)
-  offset: 25, // distance to the element when triggering the animation (default is 0)
+  offset: 15, // distance to the element when triggering the animation (default is 0)
   mobile: false, // trigger animations on mobile devices (default is true)
   live: true, // act on asynchronously loaded content (default is true)
   callback: function (box) {
