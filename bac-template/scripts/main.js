95b7851b035921cd53fa79235d8af1fe94c0262f
retojonatan <retojonatan@gmail.com>
2020-08-06 11:05:08 -0300

    disable mobile scrollreveal

--- bac-template/scripts/main.js
+++ bac-template/scripts/main.js
@@ -95,7 +95,7 @@ var wow = new WOW({
   boxClass: 'wow', // animated element css class (default is wow)
   animateClass: 'animate__animated', // animation css class (default is animated)
   offset: 25, // distance to the element when triggering the animation (default is 0)
-  mobile: true, // trigger animations on mobile devices (default is true)
+  mobile: false, // trigger animations on mobile devices (default is true)
   live: true, // act on asynchronously loaded content (default is true)
   callback: function (box) {
     // the callback is fired every time an animation is started
